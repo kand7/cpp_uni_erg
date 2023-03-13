93fceb92a95ead2a21284793306035019849c6a2
kand7 <viktor.rmn99@gmail.com>
2023-03-13 23:23:26 +0200

    Τελικό κώδικας για άσκηση 1!

--- kand7/Askisi1/askisi1.h
+++ kand7/Askisi1/askisi1.h
@@ -1,7 +1,6 @@
 // askisi1.h
 using namespace std;
 
-void twoMaxerOne(double &max1, double &max2,double array[]);
-void twoMaxerTwo(double &max1, double &max2,double array[],int size);
-double &smallest(double array[],double size);
+void twoMaxer(double array[], double size);
+double &smallest(double array[], double size);
 double *creatArray(double size);
--- kand7/Askisi1/main.cpp
+++ kand7/Askisi1/main.cpp
@@ -3,16 +3,18 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     double size = 0;
     double minimum = 0;
     cout << "Enter the size of your desired Array : ";
     cin >> size;
     printf("\n");
     double *myArray = creatArray(size);
-    minimum = smallest(myArray,size);
+    minimum = smallest(myArray, size);
+    twoMaxer(myArray, size);
     cout << "Smallest number is : " << minimum << endl;
-    cout << "Smallest number * 2 : " << minimum*2 << endl;
+    cout << "Smallest number * 2 : " << minimum * 2 << endl;
     free(myArray);
     return 0;
 }
