317cdc7e8e7bb2e54ac53b65facd553558664e4f
kand7 <viktor.rmn99@gmail.com>
2023-03-13 13:16:05 +0200

    Αλλαγές στο twoMaxerOne. Δεν χρησιμοποιούμε αναφορά στην περίπτωση 1

--- kand7/Askisi1/main.cpp
+++ kand7/Askisi1/main.cpp
@@ -3,16 +3,20 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     double size = 0;
     double minimum = 0;
+    double max1, max2 = 0;
     cout << "Enter the size of your desired Array : ";
     cin >> size;
     printf("\n");
     double *myArray = creatArray(size);
-    minimum = smallest(myArray,size);
+    minimum = smallest(myArray, size);
+    twoMaxerTwo(max1, max2, myArray, size);
+    twoMaxerOne(myArray, size);
     cout << "Smallest number is : " << minimum << endl;
-    cout << "Smallest number * 2 : " << minimum*2 << endl;
+    cout << "Smallest number * 2 : " << minimum * 2 << endl;
     free(myArray);
     return 0;
 }
