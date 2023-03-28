cdd2f03e70b897b1376cb24e260f63b633d10891
SneakyPlayerOne <soccer1067@hotmail.com>
2023-03-28 03:59:53 +0300

    Updated the code.

--- angelos/exercise1/lab1.cpp
+++ angelos/exercise1/lab1.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 void find_2l_elements(int, double &, double &, double[]);
-double &min_double(int, double[]);
+double &findMin(int, double[]);
 
 int main()
 {
     int size_of_array;
     double *array;
-    double l1, l2, min;
+    double l1, l2;
 
     cout << "Enter the size of array: ";
     cin >> size_of_array;
@@ -24,12 +24,32 @@ int main()
         cin >> array[i];
     }
 
+    cout << "\nInitial array:\n";
+    // Print the array. Should be in function.
+    cout << "[";
+    for (int i = 0; i < size_of_array - 1; i++)
+    {
+        cout << array[i] << ", ";
+    }
+    cout << array[size_of_array - 1] << "]" << endl;
+
     find_2l_elements(size_of_array, l1, l2, array);
-    min = min_double(size_of_array, array);
+    double &min = findMin(size_of_array, array); // double min = findMin(size_of_array, array); does not change the value of the array.
+    min *= 2;
 
     cout << "\nThe largest element is: " << l1 << endl;
     cout << "The 2nd largest element is: " << l2 << endl;
-    cout << "The smallest element times 2 is: " << min*2 << endl;
+    cout << "The smallest element times 2 is: " << min << endl;
+    cout << "The smallest element of the array has now been doubled.\n" << endl;
+
+    cout << "Modified array:\n";
+    // Print the array. Should be in function.
+    cout << "[";
+    for (int i = 0; i < size_of_array - 1; i++)
+    {
+        cout << array[i] << ", ";
+    }
+    cout << array[size_of_array - 1] << "]" << endl;
 
     return 0;
 }
@@ -53,21 +73,13 @@ void find_2l_elements(int size, double &l1, double &l2, double array[])
     }
 }
 
-double &min_double(int size, double array[])
+double &findMin(int size, double array[])
 {
-    double min;
-    int array_index;
-
-    min = array[0];
+    int array_index = 0;
 
     for (int i = 1; i < size; i++)
-    {
-        if (array[i] < min)
-        {
-            min = array[i];
+        if (array[i] < array[array_index])
             array_index = i;
-        }
-    }
 
     return array[array_index];
 }
