08aab90cacff936093c8e2bf6321b85370c9cabb
kand7 <viktor.rmn99@gmail.com>
2023-05-05 20:58:48 +0300

    Ολοκλήρωση και αποστολή της δέυτερης εργασίας!

--- kand7/Askisi2/ask2.h
+++ kand7/Askisi2/ask2.h
@@ -13,27 +13,33 @@ private:
     // ΜΕΘΟΔΟΙ
 public:
     // ΚΑΤΑΣΚΕΥΑΣΤΕΣ
+    // Α
     Student(const char *, string);
+    // Β
     Student(const char *, string, unsigned int);
+    // Γ (ΑΝΤΙΓΡΑΦΕΑΣ)
     Student(const Student &);
     // ΚΑΤΑΣΤΡΟΦΕΑΣ
     ~Student();
     // GETTERS
-    char *getAm();
-    string getName();
-    unsigned int getSemester();
+    char *getAm() const;
+    string getName() const;
+    unsigned int getSemester() const;
     // SETTER
     void setAm(const char *);
     void setName(string);
     void setSemester(unsigned int);
     // ΕΚΤΥΠΩΣΗ ΣΤΟΙΧΕΙΩΝ ΣΕ ΟΠΟΙΟΔΗΠΟΤΕ ΚΑΝΑΛΙ ΕΞΟΔΟΥ
-    void printInformation(ostream &);
+    void printInformation(ostream &) const;
     // ΜΕΤΑΑΥΞΗΣΗ
+    // ΟΙ ΤΥΠΟΥ ΕΠΙΣΤΡΟΦΗΣ ΕΙΝΑΙ ΟΛΟΙ Student, ΘΑ ΜΠΟΡΟΥΣΑΜΕ ΝΑ ΔΩΣΟΥΜΕ ΚΑΙ ΑΝΑΦΟΡΑ
     Student operator++(int);
-    // ΠΡΟΑΥΞΗΣΗ
+    // ΥΛΟΠΟΙΗΘΗΚΕ ΚΑΙ Η ΠΡΟΑΥΞΗΣΗ ΓΙΑ ΕΞΑΣΚΗΣΗ
     Student operator++();
     // ΤΕΛΕΣΤΗΣ +=
     Student operator+=(unsigned int);
     Student operator+=(Student &);
-    Student operator=(const Student &); // ΓΙΑ ΝΑ ΜΠΟΡΕΣΩ ΝΑ ΚΑΝΩ student4 = ++student3;
+    Student operator-=(unsigned int);
+    Student operator-=(Student &);
+    Student & operator=(const Student &); // ΓΙΑ ΝΑ ΜΠΟΡΕΣΩ ΝΑ ΚΑΝΩ student4 = ++student3;
 };
