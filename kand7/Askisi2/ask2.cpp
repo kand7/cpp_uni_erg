03f74e8cc03dc61e3a9e7d003734ea6e6f6d8477
kand7 <viktor.rmn99@gmail.com>
2023-05-05 21:02:43 +0300

    To header αρχείο μετανομάστηκε σε .old επειδή ο κύριος ΜΕΛΕΤΙΟΥ ΤΑ ΘΕΛΕΙ ΟΛΑ ΣΕ ΕΝΑ CPP ΤΕΛΙΚΑ

--- kand7/Askisi2/ask2.cpp
+++ kand7/Askisi2/ask2.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
 #include <string.h>
-#include "ask2.h"
 #include <limits.h>
+
 using namespace std;
+
+class Student
+{
+    // ΙΔΙΟΤΗΤΕΣ
+private:
+    char *am;
+    string name;
+    unsigned int semester;
+    // ΜΕΘΟΔΟΙ
+public:
+    // ΚΑΤΑΣΚΕΥΑΣΤΕΣ
+    // Α
+    Student(const char *, string);
+    // Β
+    Student(const char *, string, unsigned int);
+    // Γ (ΑΝΤΙΓΡΑΦΕΑΣ)
+    Student(const Student &);
+    // ΚΑΤΑΣΤΡΟΦΕΑΣ
+    ~Student();
+    // GETTERS
+    char *getAm() const;
+    string getName() const;
+    unsigned int getSemester() const;
+    // SETTER
+    void setAm(const char *);
+    void setName(string);
+    void setSemester(unsigned int);
+    // ΕΚΤΥΠΩΣΗ ΣΤΟΙΧΕΙΩΝ ΣΕ ΟΠΟΙΟΔΗΠΟΤΕ ΚΑΝΑΛΙ ΕΞΟΔΟΥ
+    void printInformation(ostream &) const;
+    // ΜΕΤΑΑΥΞΗΣΗ
+    // ΟΙ ΤΥΠΟΥ ΕΠΙΣΤΡΟΦΗΣ ΕΙΝΑΙ ΟΛΟΙ Student, ΘΑ ΜΠΟΡΟΥΣΑΜΕ ΝΑ ΔΩΣΟΥΜΕ ΚΑΙ ΑΝΑΦΟΡΑ
+    Student operator++(int);
+    // ΥΛΟΠΟΙΗΘΗΚΕ ΚΑΙ Η ΠΡΟΑΥΞΗΣΗ ΓΙΑ ΕΞΑΣΚΗΣΗ
+    Student operator++();
+    // ΤΕΛΕΣΤΗΣ +=
+    Student operator+=(unsigned int);
+    Student operator+=(Student &);
+    Student operator-=(unsigned int);
+    Student operator-=(Student &);
+    Student & operator=(const Student &); // ΓΙΑ ΝΑ ΜΠΟΡΕΣΩ ΝΑ ΚΑΝΩ student4 = ++student3;
+};
 // ΚΑΤΑΣΚΕΥΑΣΤΗΣ ΜΕ ΑΜ ΚΑΙ ΟΝΟΜΑΤΕΠΩΝΥΜΟ
 Student::Student(const char *am, string name)
 {
