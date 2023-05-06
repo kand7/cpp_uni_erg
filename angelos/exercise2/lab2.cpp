d2bd09560679310b251704e963f2b0c12a492f4c
SneakyPlayerOne <soccer1067@hotmail.com>
2023-05-06 21:49:41 +0300

    Final update to exercise 2.

--- angelos/exercise2/lab2.cpp
+++ angelos/exercise2/lab2.cpp
@@ -1,3 +1,10 @@
+/*
+    Ονοματεπώνυμο: Άγγελος Τζώρτζης
+    Α.Μ.: ice18390094
+    Άσκηση 2.
+    Κωδικοποιήση: UTF-8
+*/
+
 #include <iostream>
 #include <string.h>
 
@@ -19,17 +26,18 @@ public:
     unsigned int getCurrentSemester() const;
 
     // Setters.
-    void setId(char *);
+    void setId(const char *);
     void setFullName(string);
     void setCurrentSemester(unsigned int);
 
     // Methods.
-    void printInfo();
-    Student &operator++();    // Prefix operator.
-    Student &operator++(int); // Postfix operator.
-    Student &operator+=(int);
-    Student &operator-=(int);
+    void printInfo(ostream &);
+    Student &operator++();   // Prefix operator.
+    Student operator++(int); // Postfix operator.
+    Student &operator+=(unsigned int);
+    Student &operator-=(unsigned int);
 
+    // Class variables.
 private:
     char *id;
     string fullName;
@@ -38,23 +46,41 @@ private:
 
 int main(int argc, char **argv)
 {
-    Student st1("ice18390094", "Angelos Tzortzis");
+    // Δημιουργία αντικειμένων με όλους τους constructors που έχουμε.
+    Student st1("ice18390094", "Άγγελος Τζώρτζης");
     Student st2("ice00000001", "John Cena", 10);
     Student st3(st1);
 
-    st1.printInfo();
-    st2.printInfo();
-    st3.printInfo();
+    // Εκτύπωση των πληροφοριών των αντικειμένων.
+    cout << "Before changes:" << endl;
+    // Μπορούμε να επιλέξουμε όποιο κανάλι θέλουμε για έξοδο και εδώ επιλέγουμε το cout.
+    st1.printInfo(cout);
+    st2.printInfo(cout);
+    st3.printInfo(cout);
 
+    // Λειτουργία των setters.
+    st1.setCurrentSemester(2);
+    st2.setFullName("Michael Bisping");
+    st3.setId("ice00000002");
+
+    cout << endl;
+    cout << "Getters example:" << endl;
+    // Έλεγχος ειτουργία των getters.
+    cout << "st1 id: " << st1.getId() << endl;
+    cout << "st2 semester: " << st2.getCurrentSemester() << endl;
+    cout << "st3 full name: " << st3.getFullName() << endl;
+
+    // Έλεγχος υπερφόρτωσης τελεστών.
     st1++;
     ++st2;
     st3 += 5;
 
     cout << endl;
-
-    st1.printInfo();
-    st2.printInfo();
-    st3.printInfo();
+    // Εκτυπώνουμε τις πληροφορίες των αντικειμένων για να επιβεβαιώσουμε ότι έγιναν οι αλλαγές.
+    cout << "After using the overloaded operators:" << endl;
+    st1.printInfo(cout); // Tο εξάμηνο θα βγεί καθώς το αλλάξαμε σε 2 και μετα αυξήθηκε κατα 1.
+    st2.printInfo(cout);
+    st3.printInfo(cout);
 
     return 0;
 }
@@ -62,7 +88,10 @@ int main(int argc, char **argv)
 Student::Student(const char *id, string fullName)
 {
     int size = strlen(id);
+
+    // Δεσμέυουμε χώρο για τον καινούριο αριθμό μητρώου.
     this->id = new char[size + 1];
+    // Αντιγράφουμε τον αριθμό μητρώου του ορίσματος στον χώρο που δεσμέυσαμε.
     strcpy(this->id, id);
     this->fullName = fullName;
     this->currentSemester = 1;
@@ -91,8 +120,9 @@ Student::~Student()
     delete[] id;
 }
 
-void Student::setId(char *id)
+void Student::setId(const char *id)
 {
+    // Διαγράφουμε την μνήμη του παλιού αριθμού μητρώου.
     delete[] this->id;
     int size = strlen(id);
     this->id = new char[size + 1];
@@ -109,9 +139,11 @@ void Student::setCurrentSemester(unsigned int currentSemester)
     this->currentSemester = currentSemester;
 }
 
-void Student::printInfo()
+//
+void Student::printInfo(ostream &out)
 {
-    cout << "Name: " << this->fullName << ", I.D.: " << this->id << ", Semester: " << this->currentSemester << "." << endl;
+    // Γράφουμε στο κανάλι της επιλογής μας.
+    out << "Name: " << this->fullName << ", I.D.: " << this->id << ", Semester: " << this->currentSemester << "." << endl;
 }
 
 Student &Student::operator++()
@@ -120,19 +152,21 @@ Student &Student::operator++()
     return *this;
 }
 
-Student &Student::operator++(int)
+Student Student::operator++(int)
 {
+    // Εφόσον είναι τελεστής μεταύξησης θέλουμε να επιστρέφεται η τιμή πρίν αυξήσουμε το εξάμηνο.
+    Student temp = *this;
     this->currentSemester++;
-    return *this;
+    return temp;
 }
 
-Student &Student::operator+=(int addSemester)
+Student &Student::operator+=(unsigned int addSemester)
 {
     this->currentSemester += addSemester;
     return *this;
 }
 
-Student &Student::operator-=(int subSemester)
+Student &Student::operator-=(unsigned int subSemester)
 {
     this->currentSemester -= subSemester;
     return *this;
